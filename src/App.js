4968aa302b11a55e26c777082055d28ed3473330
Juan Li Puma <juanlipuma94@gmail.com>
2019-09-20 17:54:46 -0300

    Reorganize layout
    - Map to 75% width
    - Blank controls section
    - CSS to SCSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react"
-import "./App.css"
+import "./App.scss"
 import MapContainer from "./components/MapContainer"
 
 export default function App() {
   return (
     <div id="map">
-       <MapContainer />
+      <MapContainer class="container" />
+      <button style={{
+        position: 'absolute',
+        bottom: '10%',
+        left: '68%',
+      }}>Hola</button>
+      <div className="control">
+        <h1>Controls here</h1>
+      </div>
     </div>
   );
 }
