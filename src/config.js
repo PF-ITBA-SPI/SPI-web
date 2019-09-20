d3333be18748e2c86d30a651e8b6b348c19bf6bc
Juan Li Puma <juanlipuma94@gmail.com>
2019-09-20 16:04:08 -0300

    Hide API key (previous key invalidated)

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
+const MANDATORY_ENV_VARIABLES = ['REACT_APP_MAPS_API_KEY']
+MANDATORY_ENV_VARIABLES.forEach(name => { if (!process.env[name]) throw new Error(`${name} is missing, specify it in a .env file`) })
+
 export default {
-  mapsApiKey: 'AIzaSyCEZifFE48Vj672AYbGND7HhF9WfLaF-RY',
+  mapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
   apiUrl: 'https://pf-itba-spi.herokuapp.com',
 }
