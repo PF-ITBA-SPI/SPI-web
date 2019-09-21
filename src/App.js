162d12c7b9a66503aee41ae1fadcbae24386da83
Juan Li Puma <juanlipuma94@gmail.com>
2019-09-21 11:59:05 -0300

    Remove old google maps library

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react"
 import "./App.scss"
-import MapContainer from "./components/MapContainer"
 import MapContainer2 from "./components/MapContainer2"
 
 export default function App() {
   return (
-    <div id="map2">
-      {/*<MapContainer class="container" />*/}
+    <div id="map">
       <MapContainer2 className="container"
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&key=${process.env.REACT_APP_MAPS_API_KEY}`}
-        containerElement={<div style={{width: '100%', height: '100vh'}} />}
-        mapElement={<div style={{width: '75%', height: '100%'}} />}
+        containerElement={<div className="container" />}
+        mapElement={<div style={{width: '100%', height: '100%'}} />}
         loadingElement={<h1>Loading...</h1>}
       />
       <button style={{
